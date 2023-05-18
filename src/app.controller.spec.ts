46587d6b61077d9e8b1b68f5332bb7166964fceb
pruthwe <indragantipruthviraj@gmail.com>
2023-05-19 01:20:07 +0530

    Added dockerfile

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello from Nestjs!"', () => {
+      expect(appController.getHello()).toBe('Hello from Nestjs!');
     });
   });
 });
